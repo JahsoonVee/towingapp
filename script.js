a27ee02ec6c8da9f594f9da5b45b146d37cb09d3
JahsoonVee <jahsoon28@gmail.com>
2021-02-17 14:23:23 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function validate(){
+  let username = document.getElementById("staff-no").value;
+  let password = document.getElementById("staff-password").value;
+  if ( username == "00243671" && password =="2021Jamhayca"){
+    alert ("Login successful!");
+    return false;
+  }
+  }
